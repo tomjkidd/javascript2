97d8991428a2c119285caedbd9049917e8b70b49
Tom Kidd <tomjkidd@gmail.com>
2013-08-05 22:00:35 -0400

    Adding HotTowelLearning as area for playing with durandal

---
+++ HotTowelLearning/HotTowelLearning/Controllers/HotTowelController.cs
@@ -0,0 +1,16 @@
+using System.Web.Mvc;
+
+namespace HotTowelLearning.Controllers
+{
+    public class HotTowelController : Controller
+    {
+        //
+        // GET: /HotTowel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
