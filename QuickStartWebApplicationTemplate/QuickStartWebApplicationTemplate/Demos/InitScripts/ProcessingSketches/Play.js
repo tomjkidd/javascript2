43bc43959527ed9a7b64aa8ce0bbdd7c69095595
Tom Kidd <tomjkidd@gmail.com>
2013-08-05 21:54:36 -0400

    Readding project files. Adding git ignore file

---
+++ QuickStartWebApplicationTemplate/QuickStartWebApplicationTemplate/Demos/InitScripts/ProcessingSketches/Play.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+define([], function () {
+    // Processing.js site provides some sample code to create a clock
+    return function clockSketchProc(processing) {
+        var counter = 0;
+        // Override draw function, by default it will be called 60 times per second
+        processing.draw = function () {
+            // erase background
+            processing.background(224);
+
+            // draw the square
+            processing.rect(10 + counter, 10, 50, 50);
+
+            //increment the counter
+            counter = counter + 1;
+        };
+    };
+});
---
+++ QuickStartWebApplicationTemplate/QuickStartWebApplicationTemplate/Scripts/BoilerplateInit.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+require.config({
+    baseUrl: '../Scripts',
+    paths: {
+        'jquery': 'lib/jquery',
+        'knockout': 'lib/knockout',
+        'domReady': 'lib/domReady',
+        'underscore': 'lib/underscore',
+        'app': 'app',
+        'tld': 'util/tld-barebones',
+        'css': 'util/css-barebones'
+    },
+    shim: {
+        'underscore': {
+            exports: '_'
+        }
+    },
+    config: {
+        'tld': {
+            templatePath: 'templates',
+            extension: 'html'
+        },
+        'css': {
+            cssPath: 'css',
+            extension: 'css'
+        }
+    }
+});
+require(['jquery', 'knockout', 'underscore', 'util/utilities', 'app/FormattedTextBox',
+    'tld!FormattedTextBoxTemplate',
+    'css!Grid',
+    'domReady!'
+    ], function ($, ko, _, util, textBox, grid) {
+    });
