176bdf64f56f4185f7bd0c777787bbc27cb21dbd
Tom Kidd <tomjkidd@gmail.com>
2014-06-02 22:29:44 -0400

    Committing old changes...

---
+++ HotTowelLearning/HotTowelLearning/App/viewmodels/controls/modal.js
@@ -0,0 +1,16 @@
+﻿define([], function () {
+    var vm = {
+        title: 'TK Test Modal'
+    }
+
+    return vm;
+
+    ko.bindingHandlers.modal = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var check = element;
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var check = element;
+        }
+    }
+});
